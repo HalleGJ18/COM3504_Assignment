87579c57dab5407021b350b5b48842a214a85d80
Halle Gordon-Jeary <hallegj@googlemail.com>
2023-04-27 18:06:52 +0100

    access username from indexed db
    auto add username to add sighting

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'SightingsAppV2'; // update the name to get the sw to recache if resources have been updated
+const CACHE_NAME = 'SightingsAppV3'; // update the name to get the sw to recache if resources have been updated
 
 
 // Use the install event to pre-cache all initial resources.
@@ -52,20 +52,12 @@ self.addEventListener('fetch', function(event) {
 
     console.log("Url", event.request.url);
 
-    if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('/'))) {
-        event.respondWith(
-            fetch(event.request.url).catch(error => {
-                // Return the offline page
-                return caches.match('/views/index.html');
-            })
-        );
-    }
-    else{
-        // Respond with everything else if we can
-        event.respondWith(caches.match(event.request)
-            .then(function (response) {
-                return response || fetch(event.request);
-            })
-        );
-    }
+    // console.log(event.request.mode)
+    // console.log(event.request.method)
+
+    event.respondWith(caches.match(event.request)
+        .then(function (response) {
+            return response || fetch(event.request);
+        })
+    );
 });
